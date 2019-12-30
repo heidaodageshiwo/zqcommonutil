d53597b022aa2ecb6c6df5e137744f822b118d67
zhangqiang <heidaodageshiwo@163.com>
2019-12-30 17:40:29 +0800

    首次提交

---
+++ src/main/java/com/zqcommonutil/zqcommonutil/util/StringUtil.java
@@ -0,0 +1,48 @@
+package com.zqcommonutil.zqcommonutil.util;
+
+import java.util.Random;
+
+
+public class StringUtil {
+
+
+	public static boolean isEmpty(String str){
+		if(str==null||"".equals(str.trim())){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public static boolean isNotEmpty(String str){
+		if((str!=null)&&!"".equals(str.trim())){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+
+    public static String getRandomString(int length){
+        Random random = new Random();
+
+        StringBuffer sb = new StringBuffer();
+
+        for(int i = 0; i < length; ++i){
+            int number = random.nextInt(2);
+            long result = 0;
+
+            switch(number){
+                case 0:
+                    result = Math.round(Math.random() * 25 + 65);
+                    sb.append(String.valueOf((char)result));
+                    break;
+                case 1:
+
+                    sb.append(String.valueOf(new Random().nextInt(10)));
+                    break;
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/zqcommonutil/zqcommonutil/util/ZhangqiangDateUtil.java
@@ -0,0 +1,40 @@
+package com.zqcommonutil.zqcommonutil.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class ZhangqiangDateUtil {
+
+
+	public static String formatDate(Date date,String format){
+		String result="";
+		SimpleDateFormat sdf=new SimpleDateFormat(format);
+		if(date!=null){
+			result=sdf.format(date);
+		}
+		return result;
+	}
+	
+
+	public static Date formatString(String str,String format) throws Exception{
+		if(StringUtil.isEmpty(str)){
+			return null;
+		}
+		SimpleDateFormat sdf=new SimpleDateFormat(format);
+		return sdf.parse(str);
+	}
+
+	public static String getCurrentDateStr(){
+		System.out.println("===============张强测试start===============");
+		Date date=new Date();
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddhhmmssSSS");
+		System.out.println("===============张强测试end===============");
+		return sdf.format(date);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println(getCurrentDateStr());
+	}
+	
+}
