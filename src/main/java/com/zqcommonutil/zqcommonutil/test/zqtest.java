8b50d1d7913878d2f2f138c87d2494b8a1c4e8c7
zhangqiang <heidaodageshiwo@163.com>
2020-01-02 22:25:04 +0800

    首次提交0.0.5-RELEASE

---
+++ src/main/java/com/zqcommonutil/zqcommonutil/test/zqtest.java
@@ -0,0 +1,14 @@
+/*
+package com.zqcommonutil.zqcommonutil.test;
+
+import com.alipay.api.request.AlipayTradeAppPayRequest;
+
+
+public class zqtest {
+mvn clean deploy -P release
+  public static void main(String[] args) {
+    AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
+
+  }
+}
+*/
